43ddf5220093bb7d562daada8fe040749c5a4b05
aqilmarwanazhar@gmail.com <aqilmarwanazhar@gmail.com>
2025-10-26 23:43:17 +0800

    add lib/utils

---
+++ lib/utils.ts
@@ -0,0 +1,49 @@
+import { type ClassValue, clsx } from 'clsx';
+import { Metadata } from 'next';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function absoluteUrl(path: string) {
+  if (typeof window !== 'undefined') return path;
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}${path}`;
+  return `http://localhost:${process.env.PORT ?? 3000}${path}`;
+}
+
+export function constructMetadata({
+  title = 'AuraLink - AI-powered voice assistant',
+  description = 'AuraLink is an AI-powered voice assistant that makes chatting to your PDF files easy.',
+  icons = '/favicon.ico',
+  noIndex = false,
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
+  noIndex?: boolean;
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description,
+    },
+    icons,
+    metadataBase: new URL('https://auralink.vercel.app/'),
+    themeColor: '#FFF',
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  };
+}
