acd371e61eaad53222b3cf495269ed6d764646ad
aqilmarwanazhar@gmail.com <aqilmarwanazhar@gmail.com>
2025-10-27 20:59:16 +0800

    rename NavBAr

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -17,7 +17,7 @@ const Navbar = () => {
       <MaxWidthWrapper>
         <div className="flex h-14 items-center justify-between border-b border-zinc-200">
           <Link href="/" className="flex z-40 font-semibold">
-            <span>fraser</span>
+            <span>Auralink</span>
           </Link>
 
           <div className="hidden items-center space-x-4 sm:flex">
