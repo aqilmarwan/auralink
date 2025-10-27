733230740d7d71d68a988c29f8dcba60ae0a5b89
aqilmarwanazhar@gmail.com <aqilmarwanazhar@gmail.com>
2025-10-27 15:20:57 +0800

    change proto direct

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -2,9 +2,9 @@ fn main() {
   tonic_build::configure()
       .build_server(false) 
       .build_client(true)
-      .compile(
-          &["proto/audio_service.proto"],
-          &["proto"],
+      .compile_protos(  // Changed from compile() to compile_protos()
+          &["../proto/audio_service.proto"],  // Added ../ to go up one directory
+          &["../proto"],                       // Added ../ to go up one directory
       )
       .unwrap();
   
