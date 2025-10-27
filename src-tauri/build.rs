8440a158c5cc799eb529269684e029441e249651
aqilmarwanazhar@gmail.com <aqilmarwanazhar@gmail.com>
2025-10-27 14:46:56 +0800

    add build script for codegen

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,12 @@
 fn main() {
+  tonic_build::configure()
+      .build_server(false) 
+      .build_client(true)
+      .compile(
+          &["proto/audio_service.proto"],
+          &["proto"],
+      )
+      .unwrap();
+  
   tauri_build::build()
 }
